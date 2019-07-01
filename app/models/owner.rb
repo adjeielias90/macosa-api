0dd285b21826a454f8696b1e534812d3590851ef
adjeielias90 <adjeielias90@gmail.com>
2019-07-01 15:34:14 +0000

    Add company scaffold

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,9 +1,9 @@
 class Owner < ApplicationRecord
   #Validations
-  # validates_presence_of :email, :password_digest
-  # validates :email, uniqueness: true
+  validates_presence_of :email, :password_digest
+  validates :email, uniqueness: true
 
   #encrypt password
-  # has_secure_password
+  has_secure_password
   has_one :company
 end
