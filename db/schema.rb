cf1c32a5e7aa73adc454104acd96aa381c573ad2
adjeielias90 <adjeielias90@gmail.com>
2019-07-02 12:31:49 +0000

    Add isAdmin flag to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_02_115309) do
+ActiveRecord::Schema.define(version: 2019_07_02_121208) do
 
   create_table "companies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2019_07_02_115309) do
     t.string "background"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_admin"
     t.index ["company_id"], name: "index_users_on_company_id"
   end
 
