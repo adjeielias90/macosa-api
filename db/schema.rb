89d5723e8661d8e0744bae71fb9709267e222793
adjeielias90 <adjeielias90@gmail.com>
2019-07-02 11:59:39 +0000

    Commit changes to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_01_160202) do
+ActiveRecord::Schema.define(version: 2019_07_02_115309) do
 
   create_table "companies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -53,7 +53,25 @@ ActiveRecord::Schema.define(version: 2019_07_01_160202) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "firstname", null: false
+    t.string "lastname", null: false
+    t.string "title"
+    t.bigint "company_id"
+    t.string "phone"
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "background"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["company_id"], name: "index_users_on_company_id"
+  end
+
   add_foreign_key "companies", "company_types"
   add_foreign_key "companies", "owners"
   add_foreign_key "contacts", "companies"
+  add_foreign_key "users", "companies"
 end
