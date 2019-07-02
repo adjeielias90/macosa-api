cf9259957fedaabd9a9de617987f4000f97f0449
adjeielias90 <adjeielias90@gmail.com>
2019-07-02 12:07:48 +0000

    Add users logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   belongs_to :company
+  validates_presence_of :email
+  validates_uniqueness_of :email, case_sensitive: false
+  validates_format_of :email, with: /@/
 end
