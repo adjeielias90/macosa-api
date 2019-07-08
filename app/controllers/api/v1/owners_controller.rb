1e911d9c207ca856cd8f2a6998267a5e2ba3308c
adjeielias90 <adjeielias90@gmail.com>
2019-07-08 08:43:39 +0000

    Congfigure sanitize parameters

--- app/controllers/api/v1/owners_controller.rb
+++ app/controllers/api/v1/owners_controller.rb
@@ -72,6 +72,6 @@ class Api::V1::OwnersController < Api::V1::BaseController
     end
 
     def owner_params
-      params.permit(:email, :password, :name, :website)
+      params.require(:data).require(:attributes).permit(:email, :password, :name, :website)
     end
 end
