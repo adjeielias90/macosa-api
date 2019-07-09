5e468eabc73ea7669c2135d5c58479d8bcb8523f
adjeielias90 <adjeielias90@gmail.com>
2019-07-09 14:10:31 +0000

    Repair codebase

--- db/migrate/20190701153834_create_companies.rb
+++ db/migrate/20190701153834_create_companies.rb
@@ -6,7 +6,7 @@ class CreateCompanies < ActiveRecord::Migration[5.2]
       t.string :email
       t.string :website
       t.string :address
-      t.references :company_type, foreign_key: true
+      t.references :type, foreign_key: true
       t.string :background
       t.references :owner, foreign_key: true
 
