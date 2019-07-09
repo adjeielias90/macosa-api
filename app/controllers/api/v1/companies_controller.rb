6a139d3328bbe08c8971454eee35a436f08e8318
adjeielias90 <adjeielias90@gmail.com>
2019-07-09 16:08:30 +0000

    Re-enable authentication

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::CompaniesController < ApplicationController
   before_action :set_company, only: [:show, :update, :destroy]
 
   # Authorize request before processing
-  # before_action :authenticate_request!
+  before_action :authenticate_request!
   # GET /companies
   def index
     @companies = Company.all
