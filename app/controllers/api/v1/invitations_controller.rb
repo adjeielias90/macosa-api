554a5b252c5dcc4bc46eda11ed457005c0fd1485
adjeielias90 <adjeielias90@gmail.com>
2019-07-10 13:22:31 +0000

    Minor fix

--- app/controllers/api/v1/invitations_controller.rb
+++ app/controllers/api/v1/invitations_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::InvitationsController < Api::V1::BaseController
   # Authorize request before processing
-  before_action :authenticate_request!, except: [:login]
+  before_action :authenticate_request!
 
   def show
     if @current_user.is_admin?
