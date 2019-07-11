388f0c44cc68f066c9b82c1f17fff0f71d5e438e
adjeielias90 <adjeielias90@gmail.com>
2019-07-11 11:10:09 +0000

    Finally, fix logic

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -116,9 +116,9 @@ class Api::V1::UsersController < Api::V1::BaseController
           render json: {error: @user.errors}, status: :unprocessable_entity
         end
       elsif @user.email ==  @owner.email
-        if params[:is_admin]
+        if !params[:is_admin]
           render json: {errors: "You cannot change privileges for the master account"}, status: :unauthorized
-        elsif !params[:is_admin]
+        elsif params[:is_admin]
           if @user.update(update_params)
             render json: {user: @user}
           else
