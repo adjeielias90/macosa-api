efb25976f86f665fd9593839965104650c2ba128
adjeielias90 <adjeielias90@gmail.com>
2019-07-12 14:00:30 +0000

    Improve Invitations controller fix-2

--- app/controllers/api/v1/invitations_controller.rb
+++ app/controllers/api/v1/invitations_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::InvitationsController < Api::V1::BaseController
   # Authorize request before processing
-  before_action :authenticate_request!
+  before_action :authenticate_request!, except: [:show]
 
   def index
     # invitations = Invitation.all
