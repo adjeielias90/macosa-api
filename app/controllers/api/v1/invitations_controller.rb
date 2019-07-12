a1aa319238b2d928754a602843247012a359b104
adjeielias90 <adjeielias90@gmail.com>
2019-07-12 13:48:24 +0000

    Invitations controller fix

--- app/controllers/api/v1/invitations_controller.rb
+++ app/controllers/api/v1/invitations_controller.rb
@@ -52,7 +52,7 @@ class Api::V1::InvitationsController < Api::V1::BaseController
 
   def show
     if @current_user.is_admin?
-      @invitation = Invitation.where(params[:id].to_s.downcase).first
+      @invitation = Invitation.find(token: params[:id].to_s.downcase).first
       if @invitation
         if @invitation.email_confirmed?
           render json: {invitation: @invitation}, status: :ok
