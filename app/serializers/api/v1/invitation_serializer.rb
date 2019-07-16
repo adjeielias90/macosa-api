5ac2823ffd6e4df128f23d01e4098f1dae90dd87
adjeielias90 <adjeielias90@gmail.com>
2019-07-16 13:42:28 +0000

    Scrap active model serializer

--- app/serializers/api/v1/invitation_serializer.rb
+++
@@ -1,3 +0,0 @@
-class Api::V1::InvitationSerializer < ActiveModel::Serializer
-  attributes :id, :email, :firstname, :lastname, :is_admin
-end
--- app/serializers/api/v1/user_serializer.rb
+++
@@ -1,3 +0,0 @@
-class Api::V1::UserSerializer < ActiveModel::Serializer
-  attributes :id, :firstname, :lastname, :phone, :email, :is_admin, :owner_id
-end
