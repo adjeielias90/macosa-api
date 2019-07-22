89e8b076ef8c50c2b0ed9bf71d6276ab9c20395b
adjeielias90 <adjeielias90@gmail.com>
2019-07-22 09:39:27 +0000

    Refactor login action response

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -49,7 +49,7 @@ class Api::V1::UsersController < Api::V1::BaseController
       if @user && @user.authenticate(params[:password])
         auth_token = JsonWebToken.encode({user_id: @user.id})
         # use the reverse of this statement to extract user_id from token
-        render json: {access_token: auth_token}, status: :ok
+        render json: {[access_token: auth_token, user: @user]}, status: :ok
         # render json: {user: @user}, status: :ok
       else
         render json: {error: 'Invalid username/password'}, status: :unauthorized
