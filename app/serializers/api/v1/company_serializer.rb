823ea7b09e11783f2b8cff95923a7f183fddc582
adjeielias90 <adjeielias90@gmail.com>
2019-07-22 14:12:50 +0000

    Fix company serializer

--- app/serializers/api/v1/company_serializer.rb
+++ app/serializers/api/v1/company_serializer.rb
@@ -1,3 +1,3 @@
-class CompanySerializer < ActiveModel::Serializer
+class Api::V1::CompanySerializer < ActiveModel::Serializer
   attributes :id, :name, :phone, :email, :website, :address, :type_id, :background, :owner_id
 end
