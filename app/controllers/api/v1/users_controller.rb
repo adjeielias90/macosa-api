98d4aadf98a56fb32d8a0952bb3157f4fad131f8
adjeielias90 <adjeielias90@gmail.com>
2019-07-22 09:57:58 +0000

    Format login success api response

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -53,8 +53,8 @@ class Api::V1::UsersController < Api::V1::BaseController
         # render json: {user: @user}, status: :ok
 
 
-        @response = { :user => @user, :access_token => auth_token }
-        render json: { @response }, status: :ok
+        @response = [@user, auth_token]
+        render json: { access_token: @response }, status: :ok
       else
         render json: {error: 'Invalid username/password'}, status: :unauthorized
       end
