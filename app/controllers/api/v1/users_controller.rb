7f103a6d1ea5724fbe4f60018bfecb4cc2c67d64
adjeielias90 <adjeielias90@gmail.com>
2019-07-24 15:51:16 +0000

    Refactor pagination

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,8 +5,11 @@ class Api::V1::UsersController < Api::V1::BaseController
 
   def index
     @users = User.all
-    paginate json: @users, per_page: 1
+    # paginate json: @users, per_page: 1
     # render json: @users, status: :ok
+
+    @users = current_user.todos.paginate(page: params[:page], per_page: 2)
+    json_response(@users)
   end
 
   def create
