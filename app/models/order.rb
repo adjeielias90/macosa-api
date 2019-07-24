07566c296214e1440358983cd004f5e5da619e61
adjeielias90 <adjeielias90@gmail.com>
2019-07-24 13:31:27 +0000

    Refactor order model methods

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,10 +3,10 @@ class Order < ApplicationRecord
   belongs_to :account_manager
 
   def set_date!
-    self.reset_sent_at = Time.now.utc
+    self.date = Time.now.utc
   end
 
   def generate_order_number!
-    self.order_id = SecureRandom.urlsafe_base64
+    self.order_no = SecureRandom.urlsafe_base64
   end
 end
