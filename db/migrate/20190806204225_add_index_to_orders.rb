a79b04ac420e5d70ecbd3231b2a7b57e2294d889
adjeielias90 <adjeielias90@gmail.com>
2019-08-06 20:55:40 +0000

    Fix migration

--- db/migrate/20190806204225_add_index_to_orders.rb
+++ db/migrate/20190806204225_add_index_to_orders.rb
@@ -1,7 +1,8 @@
 class AddIndexToOrders < ActiveRecord::Migration[5.2]
   def change
-    add_index :orders, :customer_id
+    # add_index :orders, :customer_id
     add_index :orders, :account_manager_id
+    add_index :orders, :user_id
     add_index :orders, :currency_id
   end
 end
