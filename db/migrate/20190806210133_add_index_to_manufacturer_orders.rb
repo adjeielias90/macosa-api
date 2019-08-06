57f6a3575022e51a91b92c2daf3759ee9738576e
adjeielias90 <adjeielias90@gmail.com>
2019-08-06 21:10:24 +0000

    More indexes

--- db/migrate/20190806210133_add_index_to_manufacturer_orders.rb
+++ db/migrate/20190806210133_add_index_to_manufacturer_orders.rb
@@ -1,6 +1,6 @@
 class AddIndexToManufacturerOrders < ActiveRecord::Migration[5.2]
   def change
-      add_index :manufacturer_orders, :manufaturer_id
-      add_index :manufacturer_orders, :order_id
+      # add_index :manufacturer_orders, :manufaturer_id
+      # add_index :manufacturer_orders, :order_id
   end
 end
