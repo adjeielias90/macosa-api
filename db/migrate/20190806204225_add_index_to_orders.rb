9781e6a7a837cd91f7f0d5f4d74ad781ea6bb039
adjeielias90 <adjeielias90@gmail.com>
2019-08-06 20:51:19 +0000

    Test indexes

--- db/migrate/20190806204225_add_index_to_orders.rb
+++ db/migrate/20190806204225_add_index_to_orders.rb
@@ -1,4 +1,7 @@
 class AddIndexToOrders < ActiveRecord::Migration[5.2]
   def change
+    add_index :orders, :customer_id
+    add_index :orders, :account_manager_id
+    add_index :orders, :currency_id
   end
 end
