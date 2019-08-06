7998fbcf788b64c64f6599289f043c85ae0bbd8a
adjeielias90 <adjeielias90@gmail.com>
2019-08-06 14:40:12 +0000

    Fix error

--- app/serializers/api/v1/order_serializer.rb
+++ app/serializers/api/v1/order_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::OrderSerializer < ActiveModel::Serializer
   attributes :id, :order_no, :date, :description, :amount, :profit
-  has_many :business_orders
+  has_many :business_unit_orders
   has_many :manufacturer_orders
 end
--- db/migrate/20190806143130_add_date_to_orders.rb
+++
@@ -1,5 +0,0 @@
-class AddDateToOrders < ActiveRecord::Migration[5.2]
-  def change
-    add_column :orders, :date, :date
-  end
-end
