b1832b64b13db913e45b52f104602c079c432100
adjeielias90 <adjeielias90@gmail.com>
2019-08-06 21:06:30 +0000

    More indexes

---
+++ db/migrate/20190806210115_add_index_to_business_unit_orders.rb
@@ -0,0 +1,6 @@
+class AddIndexToBusinessUnitOrders < ActiveRecord::Migration[5.2]
+  def change
+    add_index :business_unit_orders, :business_unit_id
+    add_index :business_unit_orders, :order_id
+  end
+end
