ae9d9730207251cb3a581e165100fc8bee95a318
adjeielias90 <adjeielias90@gmail.com>
2019-08-07 07:53:13 +0000

    Fix validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ApplicationRecord
   belongs_to :customer #,optional: :true
   belongs_to :account_manager #,optional: :true
   belongs_to :currency #,optional: :true
-  has_many :manufacturer_orders, dependent: :destroy
+  has_many :manufacturer_orders, through: :manifucturers, dependent: :destroy
   has_many :business_unit_orders, dependent: :destroy
   belongs_to :user
   # belongs_to :user
@@ -24,11 +24,26 @@ class Order < ApplicationRecord
   # Modify function to check if at least 1 business_unit_order or manifucturer order exists
 
 
-  #validate :has_business_unit_order  # the name of a method we'll define below
+  validate :has_business_unit_order  # the name of a method we'll define below
   # validate :has_manufacturer_order
   # private # <-- not required, but conventional
 
+  def has_business_unit_order
+    errors.add(:base, 'must add at least one business unit') if self.business_unit_orders.blank?
+    # unless self.business_unit_order_attributes.present?
+      # since it's not an error on a single field we add an error to :base
+      # self.errors.add :base, "Order must come from at least one business unit"
+      # (of course you could be much more specific in your handling)
+    end
+  end
 
+  # def has_manufacturer_order
+  #   unless self.manufacturer_order_attributes.exists?
+  #     # since it's not an error on a single field we add an error to :base
+  #     self.errors.add :base, "Order must come from at least one manufacturer"
+  #     # (of course you could be much more specific in your handling)
+  #   end
+  # end
 
 
 
