9531937dc9f9845f7fbd48bbf95f55cd78a5d684
adjeielias90 <adjeielias90@gmail.com>
2019-08-08 02:02:33 +0000

    Rewrite orders create action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,6 +39,6 @@ class ApplicationController < ActionController::API
 
     # Sets the @current_user with the user_id from payload
     def load_current_user!
-      @current_user = User.find_by(id: payload[0]['user_id'])
+      @current_user = User.find_by(id: payload[0]['user_id']).first
     end
 end
