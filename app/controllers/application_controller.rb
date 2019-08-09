c19fc0583b000913158ef9c0ab5b2e83db79855a
adjeielias90 <adjeielias90@gmail.com>
2019-08-09 10:13:26 +0000

    Fix application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,6 +40,6 @@ class ApplicationController < ActionController::API
 
     # Sets the @current_user with the user_id from payload
     def load_current_user!
-      @current_user = User.find_by(id: payload[0]['user_id']).eager_load
+      @current_user = User.find_by(id: payload[0]['user_id'])
     end
 end
