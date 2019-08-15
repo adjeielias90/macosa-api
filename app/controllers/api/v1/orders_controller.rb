8150cf186341b8bc40ff15b1b0763bf599751afe
adjeielias90 <adjeielias90@gmail.com>
2019-08-15 10:59:18 +0000

    Refactor orders controller

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -22,7 +22,7 @@ class Api::V1::OrdersController < Api::V1::BaseController
   # Also rewrite double validation method in the order model
     # @order = @current_user.orders.create!(order_params)
     # @user = User.find(params[:user_id])
-    @order = Order.new(order_params)
+    @order = Order.create!(order_params)
     OrdersWorker.perform_async(@order)
     # @order.generate_order_number!
     # @order.set_date!
