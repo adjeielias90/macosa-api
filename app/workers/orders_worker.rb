39869d45c7981bc07ee8db02696419e254be737b
adjeielias90 <adjeielias90@gmail.com>
2019-08-15 10:50:21 +0000

    Convert order class instance to hash

--- app/workers/orders_worker.rb
+++ app/workers/orders_worker.rb
@@ -1,10 +1,10 @@
-class OrdersWorker
+class OrdersWorker < Api::V1::OrdersController
   include Sidekiq::Worker
   sidekiq_options retry: false
 
   def perform(order)
-    @order = Order.new(order)
-    @order.generate_order_number!
+    @order = Order.new(order.to_h)
+    # @order.generate_order_number!
     # @order.set_date!
     if @order.save
       render json: @order, status: :created
@@ -12,4 +12,9 @@ class OrdersWorker
       render json: @order.errors, status: :unprocessable_entity
     end
   end
+
+
+  # def order_params
+  #   params.require(:order).permit(:id, :order_no, :date, :description, :amount, :profit, :customer_id, :account_manager_id, :user_id, :currency_id, business_unit_orders_attributes: [ :id, :business_unit_id, :amount, :date, :order_id], manufacturer_orders_attributes: [:id, :manufacturer_id, :amount, :date, :order_id])
+  # end
 end
