fde485b00cb3d2d56f9daac05a1c61d7efa0e0ac
adjeielias90 <adjeielias90@gmail.com>
2019-08-17 08:57:12 +0000

    Fix order number generation

--- app/models/order.rb
+++ app/models/order.rb
@@ -14,7 +14,7 @@ class Order < ApplicationRecord
   end
 
   def generate_order_number!
-    self.order_no = SecureRandom.hex(3)
+    self.order_no = SecureRandom.hex(6)
   end
 
   # def create_order(order_params, @current_user)
