e3f6a12700801f4d56bb3f8f997f05b9e43db540
adjeielias90 <adjeielias90@gmail.com>
2019-08-20 10:45:10 +0000

    Implement filterable concern

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -10,7 +10,7 @@ class Api::V1::OrdersController < Api::V1::BaseController
   # filtering_params(params).each do |key, value|
   #   @orders = @orders.public_send(key, value) if value.present?
   # end
-  @orders = Order.filter(params.slice(:order_date, :user_id, :account_manager_id, :customer_id))
+  @orders = Order.filter(params.slice(:customer_id))
 
 
 
