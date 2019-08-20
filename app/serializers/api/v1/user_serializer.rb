573750a0cb7a12874db7fda3bf9f3f01d358c803
adjeielias90 <adjeielias90@gmail.com>
2019-08-20 05:57:48 +0000

    Fix crash

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -1,3 +1,3 @@
 class Api::V1::UserSerializer < ActiveModel::Serializer
-  attributes :id, :firstname, :lastname, :phone, :email, :is_admin, :owner_id, :user_id
+  attributes :id, :firstname, :lastname, :phone, :email, :is_admin, :owner_id
 end
