8ea87d148bf8b75820a3bc046f90bb9962b69c4c
adjeielias90 <adjeielias90@gmail.com>
2019-09-02 16:34:14 +0000

    Enforce strict validations on currency model

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,3 +1,8 @@
 class Currency < ApplicationRecord
   has_many :orders
+
+  validates :name, uniqueness: true
+  validates :name, presence: true
+  validates :symbol, uniqueness: true
+  validates :symbol, presence: true
 end
