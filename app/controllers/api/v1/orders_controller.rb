514520d7b26e06f58731f3f334f316321c568e10
adjeielias90 <adjeielias90@gmail.com>
2019-09-03 14:38:26 +0000

    Fix syntax error

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::OrdersController < Api::V1::BaseController
   # before_action :set_user, only: [:create]
   # GET /orders
   def index
-    if params[].present?
+    if params.any?
       # Test condition here to find interval scope
         # if 'to' and 'from' present,
         # Call the interval scope with:
