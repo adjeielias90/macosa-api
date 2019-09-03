05e555e7e616fc2a572c1918a58ab90ba8fd858c
adjeielias90 <adjeielias90@gmail.com>
2019-09-03 13:53:57 +0000

    Refactor orders serializer

--- app/serializers/api/v1/order_serializer.rb
+++ app/serializers/api/v1/order_serializer.rb
@@ -1,5 +1,16 @@
 class Api::V1::OrderSerializer < ActiveModel::Serializer
-  attributes :id, :order_no, :date, :description, :amount, :profit
-  has_many :business_unit_orders, root: :business_unit_orders_attributes
-  has_many :manufacturer_orders, root: :manufacturer_orders_attributes
+  attributes :id, :order_no, :date, :description, :amount, :profit, :business_unit_orders_attributes
+  def business_unit_orders_attributes
+    object.business_unit_orders.collect do |ov|
+      {
+        id: ov.id, 
+        business_unit_id: ov.business_unit_id, 
+        amount: ov.amount, 
+        date: ov.date,
+        order_id: ov.order_id
+      }
+    end
+  end
+  # has_many :business_unit_orders, root: :business_unit_orders_attributes
+  # has_many :manufacturer_orders, root: :manufacturer_orders_attributes
 end
