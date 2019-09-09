8fde2df27380bed3567ed55a157517a3746c346a
adjeielias90 <adjeielias90@gmail.com>
2019-09-09 11:29:24 +0000

    cleanup owners controller

--- app/controllers/api/v1/owners_controller.rb
+++ app/controllers/api/v1/owners_controller.rb
@@ -12,41 +12,13 @@ class Api::V1::OwnersController < Api::V1::BaseController
 
    # POST /register
    def register
-    # @owner = Owner.first
-
-    # if @owner
-    #   render json: {status: "Macosa already created, please login or reset."}, status: :unauthorized
-    # else
-    #   @owner = Owner.create(owner_params)
-    #   # Invoke send mail method here.
-
-    #   if @owner.save
-    #     response = { message: 'Macosa created successfully'}
-    #     render json: response, status: :created
-    #   else
-    #     render json: @owner.errors, status: :bad
-    #   end
-    # end
     @owner = Owner.first
-
     if @owner
       render json: {owner: @owner}, status: :ok
-      # @user = @owner.users.new(user_params)
-      # puts (user_params)
-      # if @user.save
-      #   #Invoke email function here
-      #   render json: {data: @user}, status: :created
-      # else
-      #   render json: {errors: @user.errors.full_messages}, status: :bad_request
-      # end
     else
       @owner = Owner.new(owner_params)
-
       if @owner.save
-
         @user = @owner.users.new(user_params)
-
-
         if @user.save
           @user.set_as_admin!
           render json: {owner: @user}, status: :created
@@ -54,15 +26,10 @@ class Api::V1::OwnersController < Api::V1::BaseController
         else
           render json: {data: @user.errors.full_messages}, status: :bad_request
         end
-
       else
         render json: {error: @owner.errors.full_messages}
       end
-
-      # @tenant = @owner.create_tenant(name: tenant_params[:company_name])
-
     end
-
   end
 
 
