beb84cae33485ce131da6f6086c1ba76b1ea5fee
adjeielias90 <adjeielias90@gmail.com>
2019-09-10 11:14:56 +0000

    Add initializer config for sendgrid smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,27 @@ Rails.application.configure do
   #Sendgrid 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "http://macosa-app.herokuapp.com" }
+# config/initializers/smtp.rb
+  # ActionMailer::Base.smtp_settings = {
+  #   address: 'smtp.sendgrid.net',
+  #   port: 587,
+  #   domain: 'yourdomain.com',
+  #   user_name: ENV['SENDGRID_USERNAME'],
+  #   password: ENV['SENDGRID_PASSWORD'],
+  #   authentication: :login,
+  #   enable_starttls_auto: true
+  # }
 
+
+  #if you are using the API key
+  ActionMailer::Base.smtp_settings = {
+    domain: 'http://macosa-app.herokuapp.com',
+    address:        "smtp.sendgrid.net",
+    port:            587,
+    authentication: :plain,
+    user_name:      'apikey',
+    password:       ENV.['SENDGRID_API_KEY']
+  }
 
 
   # Gmail
