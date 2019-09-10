5a48c75f50af9fc03c93844dcd95a4c8c3617484
adjeielias90 <adjeielias90@gmail.com>
2019-09-10 11:59:36 +0000

    rafactor smtp config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -107,7 +107,7 @@ Rails.application.configure do
     address:        "smtp.sendgrid.net",
     port:            587,
     authentication: :plain,
-    user_name:      'macosa.apotica@gmail.com',
+    user_name:      'apikey',
     password:       ENV['SENDGRID_API_KEY']
   }
 
