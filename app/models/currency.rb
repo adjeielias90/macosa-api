f2d584bef4066f77da3ef92133ac2254e977a4c5
adjeielias90 <adjeielias90@gmail.com>
2019-09-12 13:02:15 +0000

    Add current_user action to base controller

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,6 +1,6 @@
 class Currency < ApplicationRecord
   include PublicActivity::Model
-  tracked owner: Proc.new{ |controller, model| controller.@current_user }
+  tracked owner: Proc.new{ |controller, model| controller.current_user }
 
   acts_as_paranoid
   has_many :orders
