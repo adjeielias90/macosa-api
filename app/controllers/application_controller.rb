1243ab26299fa1dc093f9a70848eba4ae6a13553
adjeielias90 <adjeielias90@gmail.com>
2019-09-12 12:50:23 +0000

    Minor fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::API
   require 'json_web_token'
 
-  include PublicActivity::StoreController 
-
   protected
     # Validates the token and user and sets the @current_user scope
 
