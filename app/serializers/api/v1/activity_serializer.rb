3307ff8ceb8de9956302bc183bbcb05953a4d68b
adjeielias90 <adjeielias90@gmail.com>
2019-09-13 09:55:37 +0000

    Finalize public_activity

--- app/serializers/api/v1/activity_serializer.rb
+++ app/serializers/api/v1/activity_serializer.rb
@@ -1,7 +1,8 @@
 class PublicActivity::ActivitySerializer < ActiveModel::Serializer
-  attributes :id, :trackable_type, :owner_type, :owner_id, :key
+  attributes :id, :trackable_type, :owner_type, :owner_id, :key, :user_id
 
   def user_id
     object.owner_id.to_s
   end
+
 end
