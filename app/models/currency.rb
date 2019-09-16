73367d92caf1dbce7d6da621454c5e40ab010fa4
adjeielias90 <adjeielias90@gmail.com>
2019-09-16 10:44:35 +0000

    Fix app crashing

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,7 +1,7 @@
 class Currency < ApplicationRecord
   include PublicActivity::Model
   tracked owner: Proc.new { |controller, model| controller.current_user ? controller.current_user : nil }
-  tracked recipient: ->(controller, model) { model && model.user }
+  tracked recipient: ->(controller, model) { model && model.current_user }
   acts_as_paranoid
   has_many :orders
 
