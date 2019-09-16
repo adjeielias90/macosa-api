1e6543402e53e700aca9d884e30b044a5ce5e431
adjeielias90 <adjeielias90@gmail.com>
2019-09-16 19:27:02 +0000

    Refactor orders controlller

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -24,7 +24,7 @@ class Api::V1::OrdersController < Api::V1::BaseController
     @per_page = 10
     total_records = @orders.count
     # @all_orders = paginate @orders, per_page: 10
-    @orders = Order.all.paginate(per_page: 10)
+    @orders = Order.all.paginate(:per_page => 10)
     # @orders.paginate(page: 10)
     
     if (total_records % @per_page) == 0
@@ -35,7 +35,7 @@ class Api::V1::OrdersController < Api::V1::BaseController
 
 
 
-    render json: {meta: { total_pages: total_pages, total_records: total_records }, @orders }
+    render json: {meta: { total_pages: total_pages, total_records: total_records }, @orders}
     # use a custom serializer to serialize this above:
     # paginate json: @orders, per_page: 10
   end
