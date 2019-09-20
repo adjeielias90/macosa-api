27579677a862f881c2894a8716d33c23a047f88b
adjeielias90 <adjeielias90@gmail.com>
2019-09-20 11:04:32 +0000

    Refactor callback syntax

--- app/controllers/api/v1/currencies_controller.rb
+++ app/controllers/api/v1/currencies_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::CurrenciesController < Api::V1::BaseController
   before_action :set_currency, only: [:show, :update]
   before_action :authenticate_request!
-  after_save :set_currency
+  after_action :set_currency, only: [:create]
   # helper_method :current_user
   # before_action :set_user
   # GET /currencies
