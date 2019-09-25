92050ff57897fa91d2befb84873c967aef2ccafc
adjeielias90 <adjeielias90@gmail.com>
2019-09-25 11:36:14 +0000

    Fix serializer

--- app/serializers/api/v1/supplier_order_serializer.rb
+++ app/serializers/api/v1/supplier_order_serializer.rb
@@ -1,5 +1,6 @@
 class Api::V1::SupplierOrderSerializer < ActiveModel::Serializer
   attributes :id, :supplier_no, :order_no, :order_date, :amount, :eta, :delivered
-  belongs_to :order
+  # Removing this feature because it slows our API down significantly and causes timeouts
+  # belongs_to :order
   belongs_to :supplier
 end
