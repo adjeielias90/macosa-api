9a93ea4f0f37bb22a5fc9a9e1d13cde59087ebdd
adjeielias90 <adjeielias90@gmail.com>
2019-09-26 10:48:39 +0000

    Clean up supplier orders

--- app/serializers/api/v1/supplier_order_serializer.rb
+++ app/serializers/api/v1/supplier_order_serializer.rb
@@ -1,6 +1,6 @@
 class Api::V1::SupplierOrderSerializer < ActiveModel::Serializer
-  attributes :id, :supplier_no, :order_no, :order_date, :amount, :eta, :delivered
+  attributes :id, :order_id, :manufacturer_id, :supplier_no, :order_no, :order_date, :amount, :comments, :eta, :description, :delivered
   # Removing this feature because it slows our API down significantly and causes timeouts
   # belongs_to :order
-  # belongs_to :manufacturer
+  belongs_to :manufacturer
 end
