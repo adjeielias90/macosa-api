4c5144cdf70fecb64ce0538670f984a96542031e
adjeielias90 <adjeielias90@gmail.com>
2019-09-26 10:29:56 +0000

    Remove problematic sql transaction

--- db/migrate/20190925104905_add_deleted_ato_new_models.rb
+++ db/migrate/20190925104905_add_deleted_ato_new_models.rb
@@ -1,7 +1,8 @@
 class AddDeletedAtoNewModels < ActiveRecord::Migration[5.2]
   def change
+    # Column and index already exist on manufacturer model
     # add_column :manufacturers, :deleted_at, :datetime
-    add_index :manufacturers, :deleted_at
+    # add_index :manufacturers, :deleted_at
 
     add_column :supplier_orders, :deleted_at, :datetime
     add_index :supplier_orders, :deleted_at
