3ce9b25a07e9289141e23ca85698c7c9f1c6325a
adjeielias90 <adjeielias90@gmail.com>
2019-09-26 10:22:11 +0000

    Fix error prone  migrations

--- db/migrate/20190925104905_add_deleted_ato_new_models.rb
+++ db/migrate/20190925104905_add_deleted_ato_new_models.rb
@@ -1,7 +1,7 @@
 class AddDeletedAtoNewModels < ActiveRecord::Migration[5.2]
   def change
-    add_column :suppliers, :deleted_at, :datetime
-    add_index :suppliers, :deleted_at
+    add_column :manufacturers, :deleted_at, :datetime
+    add_index :manufacturers, :deleted_at
 
     add_column :supplier_orders, :deleted_at, :datetime
     add_index :supplier_orders, :deleted_at
