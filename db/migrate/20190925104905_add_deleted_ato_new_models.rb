a853891a0e8b3558a92a95f2aa2e2231c82907a5
adjeielias90 <adjeielias90@gmail.com>
2019-09-26 10:27:48 +0000

    Fix migration

--- db/migrate/20190925104905_add_deleted_ato_new_models.rb
+++ db/migrate/20190925104905_add_deleted_ato_new_models.rb
@@ -1,6 +1,6 @@
 class AddDeletedAtoNewModels < ActiveRecord::Migration[5.2]
   def change
-    add_column :manufacturers, :deleted_at, :datetime
+    # add_column :manufacturers, :deleted_at, :datetime
     add_index :manufacturers, :deleted_at
 
     add_column :supplier_orders, :deleted_at, :datetime
