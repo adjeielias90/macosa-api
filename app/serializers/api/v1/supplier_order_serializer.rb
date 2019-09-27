82fa3b5b8b34fd67809488c53d03165599d3b2ac
adjeielias90 <adjeielias90@gmail.com>
2019-09-27 16:10:29 +0000

    Remove association in supplier order serializer

--- app/serializers/api/v1/supplier_order_serializer.rb
+++ app/serializers/api/v1/supplier_order_serializer.rb
@@ -2,5 +2,5 @@ class Api::V1::SupplierOrderSerializer < ActiveModel::Serializer
   attributes :id, :order_id, :manufacturer_id, :supplier_no, :order_no, :order_date, :amount, :comments, :eta, :description, :delivered
   # Removing this feature because it slows our API down significantly and causes timeouts
   # belongs_to :order
-  belongs_to :manufacturer
+  # belongs_to :manufacturer
 end
