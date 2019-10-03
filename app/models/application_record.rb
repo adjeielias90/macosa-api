b6217dbdbef865cd950714c0b4cad8bbfb3d83c4
adjeielias90 <adjeielias90@gmail.com>
2019-10-03 10:15:11 +0000

    Finalize application record

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -4,6 +4,6 @@ class ApplicationRecord < ActiveRecord::Base
   include PublicActivity::Model
 
   # Refer to controller to understand the implementation of current_user
-  # tracked owner: Proc.new { |controller, model| controller.current_user ? controller.current_user : nil }
-  # tracked recipient: ->(controller, model) { model && model }
+  tracked owner: Proc.new { |controller, model| controller.current_user ? controller.current_user : nil }
+  tracked recipient: ->(controller, model) { model && model }
 end
