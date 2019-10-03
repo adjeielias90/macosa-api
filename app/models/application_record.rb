f1a32c205b228aedd9a6a54f914f242d588c5f74
adjeielias90 <adjeielias90@gmail.com>
2019-10-03 09:54:01 +0000

    Move notifications Proc to ApplicationRecord this is to facilitate notifications on all models reverse this change if any crashes occur

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,4 +1,9 @@
 class ApplicationRecord < ActiveRecord::Base
   acts_as_paranoid
   self.abstract_class = true
+  include PublicActivity::Model
+
+  # Refer to controller to understand the implementation of current_user
+  tracked owner: Proc.new { |controller, model| controller.current_user ? controller.current_user : nil }
+  tracked recipient: ->(controller, model) { model && model }
 end
