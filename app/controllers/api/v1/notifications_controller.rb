21a55e4081d4d82c8021912443aea86186a382dc
adjeielias90 <adjeielias90@gmail.com>
2019-10-03 09:29:59 +0000

    Fix public activity

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -31,8 +31,8 @@
 
       # end
       if params.has_key?(:user_id)
-        @user_activities = PublicActivity::Activity.find_by(owner_id: params[:user_id])
-        @activities = @user_activities.page(params[:page]).per(25)
+        activities = PublicActivity::Activity.find_by(owner_id: params[:user_id])
+        # @activities = @user_activities.page(params[:page]).per(25)
       else
         @activities = PublicActivity::Activity.all.order(created_at: :DESC).page(params[:page]).per(25)
       end
